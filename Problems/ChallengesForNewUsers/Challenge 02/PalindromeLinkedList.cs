84fe761cb369f467569d00875aba0395b1281682
Milan Rusimov <rusmil89@gmail.com>
2022-09-21 10:57:03 +0200

    !X - Improved project organization
    Why:
    
    Very important to boost your productivity

---
+++ Problems/ChallengesForNewUsers/Challenge 02/PalindromeLinkedList.cs
@@ -0,0 +1,21 @@
+namespace Problems;
+
+public class PalindromeLinkedList
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static bool IsPalindrome(ListNode head)
+    {
+        return false;
+    }
+}
---
+++ Problems/ChallengesForNewUsers/SolutionsTesting.cs
@@ -0,0 +1,10 @@
+namespace Problems;
+
+public class SolutionsTesting
+{
+    private static void Main(string[] args)
+    {
+        var a =RomanToIntegerSolution.RomanToInteger("MCMXCIV");
+        Console.WriteLine(a);
+    }
+}
