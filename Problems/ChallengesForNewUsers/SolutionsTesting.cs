11bd75a49b855da12551e11253f1bf87ca097bec
Milan Rusimov <rusmil89@gmail.com>
2022-09-28 15:37:52 +0200

    !X - Solution tester template made
    Why:
    
    Testing purpose

--- Problems/ChallengesForNewUsers/SolutionsTesting.cs
+++ Problems/ChallengesForNewUsers/SolutionsTesting.cs
@@ -1,10 +1,17 @@
+using System.Diagnostics;
+
 namespace Problems;
 
 public class SolutionsTesting
 {
     private static void Main(string[] args)
     {
-        var a =RomanToIntegerSolution.RomanToInteger("MCMXCIV");
-        Console.WriteLine(a);
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+        
+        
+
+        stopwatch.Stop();
+        Console.WriteLine("Elapsed: {0} ms", stopwatch.ElapsedMilliseconds);
     }
 }
