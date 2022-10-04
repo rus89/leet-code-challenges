fa0133cbe88b618060002a00fdd9dedc826527af
Milan Rusimov <rusmil89@gmail.com>
2022-10-04 12:58:24 +0200

    !X - Add two numbers test assertion changed
    Why:
    
    Because Assert.Equal doesn't support objects as ListNode, so I had to change it with Assert.Equivalent

--- UnitTest/TopInterviewQuestions/AddTwoNumbersTest.cs
+++ UnitTest/TopInterviewQuestions/AddTwoNumbersTest.cs
@@ -14,24 +14,32 @@ public class AddTwoNumbersTest
 
         ListNode ln1 = new ListNode(2, new ListNode(4, new ListNode(3)));
         ListNode ln2 = new ListNode(5, new ListNode(6, new ListNode(4)));
-        ListNode actual = new ListNode(7, new ListNode(0, new ListNode(8)));
-        Assert.Equal(solution.AddTwoNumbersLeetCode(ln1, ln2), actual);
+        ListNode expected = new ListNode(7, new ListNode(0, new ListNode(8)));
+        Assert.Equivalent(expected, solution.AddTwoNumbersLeetCode(ln1, ln2));
         
         ListNode ln3 = new ListNode();
         ListNode ln4 = new ListNode();
-        Assert.Equal(solution.AddTwoNumbersLeetCode(ln3, ln4), new ListNode());
+        Assert.Equivalent(new ListNode(), solution.AddTwoNumbersLeetCode(ln3, ln4));
 
-        // Solution.ListNode ln5 = new Solution.ListNode(9,
-        //     new Solution.ListNode(9,
-        //         new Solution.ListNode(9,
-        //             new Solution.ListNode(9,
-        //                 new Solution.ListNode(9,
-        //                     new Solution.ListNode(9,
-        //                         new Solution.ListNode(9)))))));
-        // Solution.ListNode ln6 = new Solution.ListNode(9,
-        //     new Solution.ListNode(9,
-        //         new Solution.ListNode(9,
-        //             new Solution.ListNode(9))));
-        // Assert.Equal(10009998, solution.AddTwoNumbers(ln5, ln6));
+        ListNode ln5 = new ListNode(9,
+            new ListNode(9,
+                new ListNode(9,
+                    new ListNode(9,
+                        new ListNode(9,
+                            new ListNode(9,
+                                new ListNode(9)))))));
+        ListNode ln6 = new ListNode(9,
+            new ListNode(9,
+                new ListNode(9,
+                    new ListNode(9))));
+        ListNode expected1 = new ListNode(8,
+            new ListNode(9,
+                new ListNode(9,
+                    new ListNode(9,
+                        new ListNode(0,
+                            new ListNode(0,
+                                new ListNode(0,
+                                    new ListNode(1))))))));
+        Assert.Equivalent(expected1, solution.AddTwoNumbersLeetCode(ln5, ln6));
     }
 }
